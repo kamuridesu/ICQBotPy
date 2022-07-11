5c4499812cd7f6344a2a7b1742a2c3543c5c5187
kamuridesu <myk.gata14@gmail.com>
2022-07-11 14:52:41 -0300

    Change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(name="ICQBot",
-      version = "0.0.1-alpha",
+      version = "0.0.2-alpha",
       author="Kamuri Amorim",
       author_email='luiz.k.amorim@gmail.com',
       url="https://github.com/kamuridesu/ICQBotPy",
