6d0a38b895777626eb69a4dcb1fe1c1128bdf7d9
kamuridesu <myk.gata14@gmail.com>
2022-07-13 15:27:51 -0300

    Change setup, add requirements and deploy pipeline

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import find_packages
 from distutils.core import setup
 
+
+def description():
+  with open("README.md", "r") as f:
+    return f.read()
+
+
+def requirements():
+  with open("requirements.txt", "r") as f:
+    return f.read().split("\n")
+
+
 setup(name="ICQBot",
-      version = "0.1.0-beta",
+      version = "0.2.0-beta",
       author="Kamuri Amorim",
       author_email='luiz.k.amorim@gmail.com',
       url="https://github.com/kamuridesu/ICQBotPy",
       download_url = "https://github.com/kamuridesu/ICQBotPy/archive/refs/heads/main.zip",
-      long_description="""ICQ bot framework""",
+      long_description=description(),
+      long_description_content_type="text/markdown",
       license="MIT",
       packages=find_packages("."),
-      install_requires=[
-        'aiohttp'
-      ]
+      install_requires=requirements()
 )
